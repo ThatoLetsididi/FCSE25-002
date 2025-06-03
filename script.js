30ec8c5a5c2b46ed0b9f0ed9ae58df3c551c8c38
ThatoLetsididi <fcse25-002@thuto.bac.ac.bw>
2025-06-03 12:01:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -60,5 +60,5 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 function showMessage(event) {
             event.preventDefault(); // Prevent form from refreshing the page
-            alert("Form submitted successfully!");
+            alert("Form submitted successfully! Thank You For Your Feedback");
         }
